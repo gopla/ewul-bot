653647a47f6953c47ccf53dbc7fabbf11857a35a
Naufal Yudhistira <yudhistn@gmail.com>
2020-05-19 06:23:43 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,8 +3,8 @@ const GoogleImages = require('google-images')
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const googleImages = new GoogleImages(
-  '016859491529718255023:kdzs054nffl',
-  'AIzaSyCcv7b5BumVDoyApas3CGuc8xVzjYzjQMg'
+  'Your Google CSE ID Here',
+  'Your Google Console API Key Here'
 )
 async function onMessage(msg) {
   let s = msg.content
@@ -33,4 +33,4 @@ async function onMessage(msg) {
 client
   .on('ready', () => console.log('I am ready!'))
   .on('message', onMessage)
-  .login(process.env.BOT_TOKEN)
+  .login('Your Discord API Key Here')

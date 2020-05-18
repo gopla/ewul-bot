d8fb3e34def581e59503da9128a77246ba096e5e
Naufal Yudhistira <yudhistn@gmail.com>
2020-05-18 15:49:20 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const GoogleImages = require('google-images')
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const googleImages = new GoogleImages(
+  '016859491529718255023:kdzs054nffl',
+  'AIzaSyCcv7b5BumVDoyApas3CGuc8xVzjYzjQMg'
+)
+async function onMessage(msg) {
+  let s = msg.content
+  if (s.match(/-nakam.*/) || s.match(/-n.*/)) {
+    s = s.match(/-nakam.*/)
+      ? s.replace('-nakam ', 'food ')
+      : s.replace('-n ', 'food ')
+
+    try {
+      const results = await googleImages.search(s)
+      var rnd = Math.floor(Math.random() * results.length)
+      msg.reply(results[rnd].url)
+    } catch (e) {
+      console.error(e)
+      msg.channel.send('Error happened, see the console')
+    }
+  }
+}
+
+client
+  .on('ready', () => console.log('I am ready!'))
+  .on('message', onMessage)
+  .login('NzExODI1MDQ5NzkwOTcxOTA0.XsIpWQ.kHhUGjNeqafr6HibpVWkT1-q5sk')
